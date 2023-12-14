e53821f6df7926237799984c08f00cd5bef7f23f
Sader <1agent319@gmail.com>
2023-12-14 10:00:10 +0200

    Week 14

---
+++ lab14/lab14.js
@@ -0,0 +1,95 @@
+import { MindARThree } from '../libs/mindar/mindar-face-three.prod.js';
+import * as faceapi from '../libs/faceapi/face-api.esm.js';
+
+const expr =
+    [
+        "neutral",
+        "happy",
+        "sad",
+        "angry",
+        "fearful",
+        "disgusted",
+        "surprised"
+    ];
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+    const start = async() =>
+    {
+        const mindarThree = new MindARThree(
+            {
+                container: document.body,
+                uiLoading: "yes",
+                uiScanning: "no",
+                uiError: "no"
+            });
+        const {renderer, scene, camera, video} = mindarThree;
+
+        await loadFaceapiNetwork();
+        console.log("models loaded");
+
+        await mindarThree.start();
+        console.log("mindar started");
+
+        await writeInfoFromFrame(renderer, scene, camera, mindarThree.video);
+    }
+    start();
+});
+
+async function loadFaceapiNetwork()
+{
+    await faceapi.nets.ssdMobilenetv1.load("../model/faceapi/");
+    await faceapi.nets.tinyFaceDetector.load("../model/faceapi/");
+    await faceapi.nets.faceLandmark68Net.load("../model/faceapi/");
+    await faceapi.nets.faceExpressionNet.load("../model/faceapi/");
+    await faceapi.nets.ageGenderNet.load("../model/faceapi/");
+};
+
+async function writeInfoFromFrame(renderer, scene, camera, video)
+{
+    let frame = 0;
+
+    renderer.setAnimationLoop(
+        async() =>
+        {
+            if (frame++ % 10 === 0)
+            {
+                const div_info = document.getElementById("info");
+                const result = await faceapi.detectSingleFace(video).withFaceLandmarks().withFaceExpressions().withAgeAndGender();
+                if (result != null)
+                {
+                    let str = "";
+                    str += "Стать " + (result["gender"] === "male" ? "чоловіча" : "жіноча") + " ";
+                    str += "(ймовірність правильного визначення - " + Math.round(100 * parseFloat(result["genderProbability"])) + "%" + ")" + "\<br\>";
+                    str += "Вік - " + Math.round(parseFloat(result["age"])) + " років" + "<br>";
+                    str += checkEmotion(result);
+                    div_info.innerHTML = str;
+                }
+            }
+        }
+    )
+    renderer.render(scene, camera);
+}
+
+function checkEmotion(result)
+{
+    const expressions = result["expressions"];
+    if (expressions == null)
+    {
+        console.log("expressions is null");
+        return "";
+    }
+    let emotion = expressions[expr[0]];
+    let probability = 0;
+
+    for (let index = 0; index < expr.length; index++)
+    {
+        if (expressions[expr[index]] > probability)
+        {
+            probability = expressions[expr[index]];
+            emotion     = expr[index];
+        }
+    }
+
+    return emotion + " (ймовірність правильного визначення - " + Math.round(100 * probability) + "%" + ")" + "<br>";
+}
