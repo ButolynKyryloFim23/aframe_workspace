f407b41f88a0dba68854338f968669f0f3ac87c1
Sader <1agent319@gmail.com>
2023-12-16 16:55:31 +0200

    * В 7 роботі прибрив залежніть в скрипті від A-Frame

--- lab07/lab07.js
+++ lab07/lab07.js
@@ -1,23 +1,26 @@
-AFRAME.registerComponent('main', {
-
-    tick: function (time, deltaTime)
-    {
-        for (let index = 1; index <= 3; index++)
-        {
-            const electron = document.querySelector('#electron' + index);
-            if (electron == null)
-            {
-                continue;
-            }
-            let radius = parseInt  (electron.getAttribute("animationRadius"));
-            let speed  = parseFloat(electron.getAttribute("animationSpeed" ));
-
-            electron.setAttribute('position',
-                {
-                x: radius * Math.cos(speed * time / 1000),
-                y: 0,
-                z: radius * Math.sin(speed * time / 1000),
-            });
-        }
-    }
+document.addEventListener("DOMContentLoaded", () =>
+{
+    startRotation();
 });
+
+function startRotation()
+{
+    for (let index = 1; index <= 3; index++)
+    {
+        const electron = document.querySelector('#electron' + index);
+        if (electron == null)
+        {
+            continue;
+        }
+        let radius = parseInt  (electron.getAttribute("animationRadius"));
+        let speed  = parseFloat(electron.getAttribute("animationSpeed" ));
+
+        electron.setAttribute('position',
+            {
+                x: radius * Math.cos(speed * Date.now() / 1000),
+                y: 0,
+                z: radius * Math.sin(speed * Date.now() / 1000),
+            });
+    }
+    requestAnimationFrame(startRotation);
+}
